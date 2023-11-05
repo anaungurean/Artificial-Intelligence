90e40208ea0f4c832bafb000d0725d9aa89a809e
anaungurean <100145653+anaungurean@users.noreply.github.com>
2023-11-05 22:53:36 +0200

    Update Homework4.py

--- Lab4/Homework4.py
+++ Lab4/Homework4.py
@@ -50,6 +50,70 @@ class NumberScramble:
     def switch_player(self):
         self.current_player = 'AI' if self.current_player == 'Player' else 'Player'
 
+    def heuristic(self):
+        '''
+        Pentru AI, o direcție deschisă este un triplet unde niciunul dintre numere nu a fost ales de jucătorul uman,
+        Pentru jucătorul uman este un triplet unde niciunul dintre numere nu a fost ales de AI.
+        '''
+        ai_score = 0 #nr de directii deschise pentru AI
+        player_score = 0 #nr de directii deschise pentru player
+        for triplet in self.triplets_sum_15:
+            if all(num in self.available_numbers or num in self.player_moves['AI'] for num in triplet): #verificam daca toate numerele din triplet fie nu au fost deja alese fie au fost alese de jucatorul resp.
+                ai_score += 1
+            if all(num in self.available_numbers or num in self.player_moves['Player'] for num in triplet):
+                player_score += 1
+        return ai_score - player_score # >0 -> avantaj AI (are mai multe directii deschise) <0 -> avantaj Player = 0 ambii jucători au același număr de oportunități de a câștiga.
+
+    def minmax(self, depth, is_max_player): #scorul estimat al starii curente a jocului
+
+        if self.is_game_over():
+            if self.is_winner('AI'):
+                return 10
+            elif self.is_winner('Player'):
+                return -10
+            else:
+                return 0
+
+        if depth == 0:
+            return self.heuristic()
+
+        if is_max_player:
+            best_score = -np.inf
+            for number in self.available_numbers:
+                self.make_move(number,'AI')
+                score = self.minmax(depth - 1, False)
+                self.undo_move(number, 'AI')
+                best_score = max(best_score, score)
+            return best_score
+        else:
+            best_score = np.inf
+            for number in self.available_numbers:
+                self.make_move(number, 'Player')
+                score = self.minmax(depth - 1, True)
+                self.undo_move(number, 'Player')
+                best_score = min(best_score, score)
+            return best_score
+
+    def undo_move(self, number, player):
+        self.player_moves[player].remove(number)
+        self.available_numbers.append(number)
+        self.available_numbers.sort()
+
+    def find_best_move_for_AI(self):
+        best_score = -np.inf
+        best_move = None
+        for number in self.available_numbers:
+            self.make_move(number, 'AI')
+            score = self.minmax(2,False) # false deoarece urmatoare mutare este a adevarsarului
+            self.undo_move(number,'AI')
+            if score > best_score:
+                best_score = score
+                best_move = number
+        return best_move
+
+
+
+
 def run_game():
     game = NumberScramble()
     while not game.is_game_over():
@@ -59,7 +123,7 @@ def run_game():
             while not valid_entry:
                 try:
                     player_move = int(input("Choose a number (1-9): "))
-                    if player_move in range(1,10):
+                    if player_move in range(1, 10):
                         valid_entry = True
                     else:
                         print("Invalid number. Please choose a number between 1 and 9.")
@@ -69,10 +133,13 @@ def run_game():
                 print("Invalid move, number is not available. Try again.")
                 continue
         else:
-            ai_move = np.random.choice(game.available_numbers)
-            print("\nAI's turn.")
-            print("AI chooses: " + str(ai_move))
-            game.make_move(ai_move, 'AI')
+            ai_move = game.find_best_move_for_AI()
+            if ai_move is not None:
+                print("\nAI's turn.")
+                print("AI chooses: " + str(ai_move))
+                game.make_move(ai_move, 'AI')
+            else:
+                print("AI passes its turn (no valid moves).")
     game.switch_player()
 
     winner = game.get_winner()
@@ -82,18 +149,5 @@ def run_game():
         print("\nThe winner is: " + winner)
 
 
-
 if __name__ == "__main__":
     run_game()
-
-
-
-
-
-
-
-
-
-
-
-
