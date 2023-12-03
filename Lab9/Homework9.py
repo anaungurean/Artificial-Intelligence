a6c3b0491f66c557e7cf97157221753d8f0e9b84
anaungurean <100145653+anaungurean@users.noreply.github.com>
2023-12-03 21:05:16 +0200

    corectare

--- Lab9/Homework9.py
+++ Lab9/Homework9.py
@@ -1,3 +1,5 @@
+import random
+
 import numpy as np
 class QLearningAgent:
     def __init__(self, num_rows, num_cols, initial_state, goal_state, wind, learning_rate, discount_factor, num_episodes ): #a
@@ -17,7 +19,7 @@ class QLearningAgent:
         action_deltas = [(1, 0), (-1,0), (0,-1), (0,1)]  # jos, sus, stanga, dreapta
         delta = action_deltas[action]  # aplicăm modificarea pentru acțiunea dată
 
-        wind_effect = self.wind[state[0]] if 0 <= state[0] < len(self.wind) else 0
+        wind_effect = self.wind[state[1]] if 0 <= state[1] < len(self.wind) else 0
         delta = (delta[0] - wind_effect, delta[1])
 
         # Asigurăm că agentul nu iese din grid
@@ -26,33 +28,48 @@ class QLearningAgent:
 
         return next_state
 
-    def q_learning(self):
+    def q_learning(self, epsilon=0.1):
         for episode in range(self.num_episodes):
             current_state = self.initial_state
+            total_reward = 0  # Initialize total_reward for the current episode
 
             while current_state != self.goal_state:
-                action = np.argmax(self.q_table[current_state])
-                next_state = self.take_action(current_state, action)
+                # Epsilon-greedy strategy for action selection
+                if random.uniform(0, 1) < epsilon:
+                    action = random.choice([0, 1, 2, 3])  # Explore by choosing a random action
+                else:
+                    action = np.argmax(self.q_table[current_state])  # Exploit by choosing the best action
 
+                next_state = self.take_action(current_state, action)
                 reward = -1
                 if next_state == self.goal_state:
-                    reward = float('inf')
+                    reward = 100  # Use a large but finite reward for reaching the goal
 
                 best_next_action = np.argmax(self.q_table[next_state])
                 self.q_table[current_state][action] += self.learning_rate * (
                         reward + self.discount_factor * self.q_table[next_state][best_next_action]
                         - self.q_table[current_state][action])
+
+                total_reward += reward  # Accumulate the reward for the current episode
                 current_state = next_state
-            current_state = self.initial_state
 
-    def get_optimal_policy(self):
-        optimal_policy = np.zeros((self.num_rows, self.num_cols), dtype=int)
+            # Print total reward obtained in each episode
+            print(f"Episode {episode + 1}, Total Reward: {total_reward}")
 
-        for row in range(self.num_rows):
-            for col in range(self.num_cols):
-                optimal_policy[row, col] = np.argmax(self.q_table[row, col, :])
+        current_state = self.initial_state
 
-        return optimal_policy
+    def get_optimal_path(self):
+        current_state = self.initial_state
+        path = [current_state]
+
+        while current_state != self.goal_state:
+            action = np.argmax(self.q_table[current_state])
+            next_state = self.take_action(current_state, action)
+            path.append(next_state)
+            current_state = next_state
+
+        return path
+
 
 if __name__ == '__main__':
 
@@ -63,23 +80,22 @@ if __name__ == '__main__':
     wind = [0, 0, 0, 1, 1, 1, 2, 2, 1, 0]
     learning_rate = 0.1
     discount_factor = 0.9
-    num_episodes = 1000
+    num_episodes = 200
 
     # Inițializarea agentului
     agent = QLearningAgent(num_rows, num_cols, initial_state, goal_state, wind, learning_rate, discount_factor, num_episodes)
 
     # Testarea funcției take_action
     actions = [0, 1, 2, 3]  # 0: jos, 1: sus, 2: stanga, 3: dreapta
+    agent.q_learning()
 
     for action in actions:
         next_state = agent.take_action(initial_state, action)
         print(f"Action: {action}, Next State: {next_state}")
 
-    # Training the Q-learning agent
-    agent.q_learning()
-    optimal_policy = agent.get_optimal_policy()
-    print("Optimal Policy:")
-    print(optimal_policy)
-
-
+    # Obținerea și afișarea drumului optim
+    optimal_path = agent.get_optimal_path()
+    print("Optimal Path:")
+    for state in optimal_path:
+        print(state)
 
