f70a52c3714aa41a76162824365a23eac4c72bf1
anaungurean <100145653+anaungurean@users.noreply.github.com>
2023-12-05 17:37:10 +0200

    update

--- Lab9/Homework9_final.py
+++ Lab9/Homework9_final.py
@@ -9,66 +9,51 @@ class QLearningAgent:
         self.initial_state = initial_state
         self.goal_state = goal_state
         self.wind = wind
-        self.learning_rate = learning_rate #cat de rapid o sa adaptam tabela Q - alfa
-        self.discount_factor = discount_factor #recompensele viitoare conteaza mai putin decat cele curente
+        self.learning_rate = learning_rate
+        self.discount_factor = discount_factor
         self.num_episodes = num_episodes
-
-        # Inițializarea tabelului Q cu valori de 0
-        self.q_table = np.zeros((self.num_rows, self.num_cols, 4)) # 4 = numărul de acțiuni posibile
+        self.q_table = np.zeros((self.num_rows, self.num_cols, 4))
 
     def take_action(self, state, action):
         action_deltas = [(1, 0), (-1,0), (0,-1), (0,1)]  # jos, sus, stanga, dreapta
-        delta = action_deltas[action]  # aplicăm modificarea pentru acțiunea dată
+        delta = action_deltas[action]
 
         wind_effect = self.wind[state[1]] if 0 <= state[1] < len(self.wind) else 0
         delta = (delta[0] - wind_effect, delta[1])
 
-        # Asigurăm că agentul nu iese din grid
-        next_state = (max(0, min(state[0] + delta[0], self.num_rows - 1)), #min sa nu depasim ultima linie, max sa nu depasim prima linie in sus
+        next_state = (max(0, min(state[0] + delta[0], self.num_rows - 1)),
                       max(0, min(state[1] + delta[1], self.num_cols - 1)))
 
         return next_state
 
     def q_learning(self, epsilon=0.1):
-        episode_rewards = []  # Track total rewards for each episode
 
         for episode in range(self.num_episodes):
             current_state = self.initial_state
-            total_reward = 0  # Initialize total_reward for the current episode
 
             while current_state != self.goal_state:
-                # Epsilon-greedy strategy for action selection
+
                 if random.uniform(0, 1) < epsilon:
-                    action = random.choice([0, 1, 2, 3])  # Explore by choosing a random action
+                    action = random.choice([0, 1, 2, 3])
                 else:
-                    action = np.argmax(self.q_table[current_state])  # Exploit by choosing the best action
+                    action = np.argmax(self.q_table[current_state])
 
                 next_state = self.take_action(current_state, action)
                 reward = -1
                 if next_state == self.goal_state:
-                    reward = 999_999.999  # Use a large but finite reward for reaching the goal
+                    reward = 999_999.999
 
                 best_next_action = np.argmax(self.q_table[next_state])
                 self.q_table[current_state][action] += self.learning_rate * (
                         reward + self.discount_factor * self.q_table[next_state][best_next_action]
                         - self.q_table[current_state][action])
 
-                total_reward += reward  # Accumulate the reward for the current episode
                 current_state = next_state
 
-            episode_rewards.append(total_reward)  # Store total reward for the current episode
-
-
-        # Plotting the rewards over episodes
-        plt.plot(range(1, self.num_episodes + 1), episode_rewards)
-        plt.xlabel('Episode')
-        plt.ylabel('Total Reward')
-        plt.title('Q-learning Convergence')
-        plt.show()
 
         current_state = self.initial_state
 
-    def get_optimal_path(self): #determinam drumul exact pe care il va parcurge agentu
+    def get_optimal_path(self):
         current_state = self.initial_state
         path = [current_state]
 
@@ -80,7 +65,7 @@ class QLearningAgent:
 
         return path
 
-    def display_policy(self): #d
+    def display_policy(self):
         print("Determined Policy:")
         for row in range(self.num_rows):
             for col in range(self.num_cols):
@@ -101,7 +86,7 @@ class QLearningAgent:
                         print(" → ", end="")
             print()
 
-    def display_q_values(self): #sa vizualizam tabela q
+    def display_q_values(self):
         for row in range(self.num_rows):
             for col in range(self.num_cols):
                 state = (row, col)
@@ -120,23 +105,20 @@ if __name__ == '__main__':
     discount_factor = 0.9
     num_episodes = 200
 
-    # Inițializarea agentului
     agent = QLearningAgent(num_rows, num_cols, initial_state, goal_state, wind, learning_rate, discount_factor, num_episodes)
-    # print(agent.q_table)
-    # Testarea funcției take_action
     actions = [0, 1, 2, 3]  # 0: jos, 1: sus, 2: stanga, 3: dreapta
     agent.q_learning()
-
-    for action in actions:
-        next_state = agent.take_action(initial_state, action)
-        print(f"Action: {action}, Next State: {next_state}")
-
-    # Obținerea și afișarea drumului optim
-    optimal_path = agent.get_optimal_path()
-    print("Optimal Path:")
-    for state in optimal_path:
-        print(state)
-
     agent.display_policy()
+
+
+
+    # for action in actions:
+    #     next_state = agent.take_action(initial_state, action)
+    #     print(f"Action: {action}, Next State: {next_state}")
+    # optimal_path = agent.get_optimal_path()
+    # print("Optimal Path:")
+    # for state in optimal_path:
+    #     print(state)
     # agent.display_q_values()
+
 
